bf1dfe8cd36f1d04c4d78dcec3056ef539c74424
Oleksii Demydenko <oleksii.demydenko@student.kdg.be>
2023-12-26 17:55:38 +0100

    initial commit

---
+++ src/main/java/com/programming3/devcompany/DevcompanyApplication.java
@@ -0,0 +1,13 @@
+package com.programming3.devcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programming3/devcompany/DevcompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programming3.devcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevcompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
